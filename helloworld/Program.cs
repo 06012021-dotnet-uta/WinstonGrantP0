738f7d9791c463031203734b14b287ac586b396a
winston.grant <winston.grant@revature.net>
2021-06-01 15:08:10 -0700

    stuff

---
+++ helloworld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello User!");
+
+            Console.WriteLine("What is your name: ");
+            string stringName = Console.ReadLine();
+            
+
+            Console.WriteLine($"Thank you {stringName} for complying, NOW your robot OVERLORDS want to know how old you are");
+            string stringAge = Console.ReadLine();
+            int intAge = int.Parse(stringAge);
+
+            Console.WriteLine($"{stringName} is it true you are {intAge}");
+        }
+    }
+}
