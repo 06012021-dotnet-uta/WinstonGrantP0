6117aae2f3f27bba9409e713221e73b03175bb05
winston.grant <winston.grant@revature.net>
2021-06-14 18:43:51 -0700

    save point

--- P0full/POVS/POMain/BusinessLogic/LogicClass.cs
+++ P0full/POVS/POMain/BusinessLogic/LogicClass.cs
@@ -1,24 +1,30 @@
 ﻿using System;
+using DataBase;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace BusinessLogic
 {
 	public class LogicClass
 	{
+		P0Context context = new P0Context();
 		public void CreateAnAccount()
 		{
 			//these will be for the database creation of a new customer
 			string LName;
 			string FName;
 
-			//get first name
+			//gets first name
 			do
 			{
 				Console.WriteLine($"\tWelcom to the world of pokemon young trainer! \n\t what is your firstname: ");
 				string rawFName = Console.ReadLine();
 				FName = rawFName.Trim();
+				//input validation for firstname
+				if (FName.Length > 20 && FName == null)
+					{Console.WriteLine($"\tplease give us a name under 20 characters that's also not empty please"); }
 			}
-			while (FName.Length < 20 && FName != null);
+			while (FName.Length > 20 || FName == null);
 
 			//get last name
 			do
@@ -26,14 +32,31 @@ namespace BusinessLogic
 				Console.WriteLine($"\t{FName} what is your last name? ");
 				string rawLName = Console.ReadLine();
 				LName = rawLName.Trim();
+				//input validation for lastname
+				if (LName.Length > 20 || LName == null)
+				{ Console.WriteLine($"\tplease give us a name under 20 characters that's also not empty please"); }
 			}
-			while (LName.Length < 20 && LName != null);
+			while (LName.Length > 20 || LName == null);
 
-			Console.WriteLine($"\tHello {FName} {LName} welcome to the store"!);
+			Console.WriteLine($"\tHello {FName} {LName} Congradulations on creating an acount!");
 
 			//
 			//put a method that comits here!
+			ComitCustomer(FName,LName);
 			//
+		}
+
+		public void ComitCustomer(string FName, string LName) 
+		{
+			var newcust = new DataBase.Customer();
+
+			newcust.Fname = FName;
+			newcust.Lname = LName;
+
+			context.Add(newcust);
+			context.SaveChanges();
+
+
 		}
 	}
 }
--- P0full/POVS/POMain/POMain/HeyListen.cs
+++ P0full/POVS/POMain/POMain/HeyListen.cs
@@ -9,6 +9,8 @@ using BusinessLogic;
 public class HeyListen
 {
 	LogicClass Logic = new LogicClass();
+
+	//this is the opening asking for log in info
 	public void LogInQ()
 	{
 		//  Hello  //
@@ -18,13 +20,42 @@ public class HeyListen
 		string resoponse = Console.ReadLine();
 		string editedResponse = resoponse.Trim().ToLower();
 
-		//
+		//if y push to log in method else push to create acount method
 		if (editedResponse.StartsWith($"y"))
 		{
+			//go to log in
 			Console.WriteLine($""); 
 		}
+		//go to create an acount
+		Logic.CreateAnAccount();
 		Console.WriteLine($""); 
 	}
 
-	
+	//
+	public bool StillShopping()
+	{
+
+		Console.WriteLine($"\tThank you for choosing us for your poke needs\n\t if you would like to keep shopping type yes");
+
+		string resoponse = Console.ReadLine();
+		string editedResponse = resoponse.Trim().ToLower();
+		if (editedResponse.StartsWith('y'))
+			//keeping running the program
+		{ return true; }
+		//laughs in exit
+		  return false; 
+	}
+
+			 
+
+
+	public void exit() 
+	{
+		Console.WriteLine("Hey, \n have a good day!");
+		//if (exit.ToLower() == "exit")
+
+
+	}
+
+
 }
